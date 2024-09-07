4fa229614809f9baae8ad93e8e9d422377e4d54f
Ant <toha2217@mail.ru>
2024-09-07 12:49:01 +0300

    First homework done

---
+++ First_hometask/Program.cs
@@ -0,0 +1,7 @@
+﻿//string? a = Console.ReadLine();
+//Console.WriteLine(a);
+int a = 10, b=12, c=8, d=6;
+double product = a * b;
+double sum = c + d;
+double quotient = product/sum;
+Console.WriteLine(quotient);
