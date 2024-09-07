9cc31a5ac981af1ad9967636cc345b80179cb4b3
Ant <toha2217@mail.ru>
2024-09-07 13:56:00 +0300

    Added Tasks folder and first task

---
+++ Lesson_tasks/kettlebells/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 5, b = 67, c = 5, d = 62, e = 14;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
