586b6f683f383e84825b7b1ec06fa50048f0aa64
Ant <toha2217@mail.ru>
2024-09-08 03:44:11 +0300

    Updated kettlebells and new folder array!

---
+++ Lesson_tasks/array/Program.cs
@@ -0,0 +1,33 @@
+﻿// int[] arr = {1,41,25,12,17};
+// Console.WriteLine(arr[0]);
+// Console.WriteLine(arr[1]);
+// Console.WriteLine(arr[2]);
+// Console.WriteLine(arr[3]);
+// Console.WriteLine(arr[4]);
+
+// int n = 10, i = 0;
+// int[] arr2 = new int[n];
+// while (i < n)
+// {
+//     arr2[i] = i + 1;
+//     Console.WriteLine(arr2[i]);
+//     i = i + 1;
+// }
+
+int i = 0;
+int[] arr = {12, 42, 21, 41, 32, 16, 15, 61, 31, 9};
+while (i < arr.Length)
+{
+    if (arr[i]%2==0)
+    {
+    Console.WriteLine("Чётый элемент:", arr[i]);
+    Console.WriteLine(arr[i]);
+    i = i + 1;
+    }
+    else
+    {
+        Console.WriteLine("Нечётный элемент: ", arr[i]);
+        Console.WriteLine (arr[i]);
+        i = i + 1;
+    }
+}
--- Lesson_tasks/kettlebells/Program.cs
+++ Lesson_tasks/kettlebells/Program.cs
@@ -1,20 +1,33 @@
-﻿int a = 5, b = 67, c = 5, d = 62, e = 14;
-int max = a;
+﻿// int a = 5, b = 67, c = 5, d = 62, e = 14;
+// int max = a;
 
-if (b > max)
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// if (d > max)
+// {
+//     max = d;
+// }
+// if (e > max)
+// {
+//     max = e;
+// }
+// Console.WriteLine(max);
+
+int i = 0;
+int[] arr = { 2, 5, 12, 6, 77 };
+int max = arr[i];
+while (i < arr.Length)
 {
-    max = b;
-}
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (e > max)
-{
-    max = e;
+    if (arr[i] > max)
+   {
+        max = arr[i];
+    }
+    i = i + 1;
 }
 Console.WriteLine(max);
