d1009a3468e3bf34d717fb3174788fd7abc82bda
Ant <toha2217@mail.ru>
2024-09-08 05:20:56 +0300

    kettlebells update (more cycles)

--- Lesson_tasks/kettlebells/Program.cs
+++ Lesson_tasks/kettlebells/Program.cs
@@ -19,15 +19,37 @@
 // }
 // Console.WriteLine(max);
 
-int i = 0;
+// int i = 0;
+//int[] arr = { 2, 5, 12, 6, 77 };
+// int max = arr[i];
+// while (i < arr.Length)
+// {
+//     if (arr[i] > max)
+//    {
+//         max = arr[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+// int[] arr = { 2, 5, 12, 6, 77 };
+// int max = arr[0];
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > max)
+//    {
+//         max = arr[i];
+//    }
+// }
+// Console.WriteLine(max);
+
 int[] arr = { 2, 5, 12, 6, 77 };
-int max = arr[i];
-while (i < arr.Length)
+int max = arr[0];
+foreach (int i in arr)
 {
-    if (arr[i] > max)
-   {
-        max = arr[i];
+    if (i > max)
+    {
+        max = i;
     }
-    i = i + 1;
 }
 Console.WriteLine(max);
