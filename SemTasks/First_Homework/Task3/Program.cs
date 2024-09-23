23c44f5f236b06cf31632362d21d00c8edd64f4f
Ant <toha2217@mail.ru>
2024-09-23 19:46:20 +0300

    First homework is done

---
+++ SemTasks/First_Homework/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Answer {
+static void CheckNumber(int number)
+{
+// Введите свое решение ниже
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    Console.WriteLine("Введенное число является положительным.");
+}
+else if (num < 0)
+{
+    Console.WriteLine("Введенное число является отрицательным.");
+}
+else
+{
+Console.WriteLine("Введенное число является нулем.");
+}
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 7;
+}
+CheckNumber(number);
+}
+}
---
+++ SemTasks/First_Homework/Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 0)
+{
+    Console.WriteLine("Ошибка. Введенное число не является положительным!");
+}
+Console.WriteLine("Ряд нечётных числа от 1 до вашего числа(включительно):");
+for (int i = 1; i <= num; i = i + 2)
+{
+    Console.Write($"{i} ");
+}
